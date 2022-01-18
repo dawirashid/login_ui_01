a636a4e55a5f9c942e962ba6c81e48b46c27cbf9
dawirashid <77828860+dawirashid@users.noreply.github.com>
2022-01-18 13:08:46 +0800

    add widget ClipRRect()

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,7 @@
-
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,8 +13,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor:  Color(0xc1c57253),
+    return Scaffold(
+      backgroundColor: const Color(0xeb050404),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(9)),
+              child: SignInButton(
+                Buttons.Email,
+                text: 'Continue with email',
+                onPressed: (){},
+              ),
+            ),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(9)),
+              child: SignInButton(
+                Buttons.Google,
+                text: 'Continue with Google ',
+                onPressed: (){},
+              ),
+            ),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(9)),
+              child: SignInButton(
+                Buttons.Facebook,
+                text: 'Continue with Facebook',
+                onPressed: (){},
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
